9d8594ef3346bd7f678678116450c93636b22ba3
zhangjiaqi24 <1us_z8wo1mw0ml@dingtalk.com>
2023-09-11 09:29:57 +0800

    feature: 修改端口号

--- utils/connection.js
+++ utils/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 //mongodb://127.0.0.1:27017/link_list
-mongoose.connect(`mongodb://localhost:27017/blog`, {
-// mongoose.connect(`mongodb://root:root!2023@127.0.0.1:27017/electric_charge_one?authMechanism=DEFAULT&authSource=admin`, {
+// mongoose.connect(`mongodb://localhost:27017/blog`, {
+mongoose.connect(`mongodb://root:root!2023@127.0.0.1:27017/blog?authMechanism=DEFAULT&authSource=admin`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (err) => {
